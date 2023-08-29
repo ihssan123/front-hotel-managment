e4d6c6c41456bddbdff2f326d0957d9cce9cfe58
IKHLAS <ikhlasqassimi1955@gmail.com>
2023-08-29 11:14:06 +0100

    ajouter chi profile

--- app/(main)/uikit/formlayout/page.tsx
+++ app/(main)/uikit/formlayout/page.tsx
@@ -3,8 +3,11 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import axios from 'axios';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Dropdown } from 'primereact/dropdown';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface DropdownItem {
     name: string;
@@ -12,6 +15,27 @@ interface DropdownItem {
 }
 
 const FormLayoutDemo = () => {
+    const token=localStorage.getItem('accessToken');
+    const[userObject,setUserObject]=useState([])
+  
+
+    useEffect(() => {
+        const user = localStorage.getItem('user');
+        if (user !== null) {
+            setUserObject(JSON.parse(user));
+        } else {
+            console.log("erreur");
+        }
+    }, []); 
+    const handleInputChange = (field: string, value: string) => {
+        setUserObject(prevUserObject => ({
+            ...prevUserObject,
+            [field]: value
+        }));
+    };
+
+
+
     const [dropdownItem, setDropdownItem] = useState<DropdownItem | null>(null);
     const dropdownItems: DropdownItem[] = useMemo(
         () => [
@@ -22,122 +46,60 @@ const FormLayoutDemo = () => {
         []
     );
 
+
+
+    const handleSave = (id:number) => {
+        const url = `http://localhost:8080/api/v1/user/${id}`;
+    
+        axios.put(url, userObject, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((response: any) => {
+            console.log(response.data);
+            localStorage.setItem('user', JSON.stringify(userObject));
+            toast.success('Update successful'); // Show success toast
+        })
+        .catch((error: any) => {
+            console.error(error);
+            toast.error('Update failed'); // Show error toast
+        });
+    };
+    
+       /* setDropdownItem(dropdownItems[0]);
+    }, [dropdownItems]);
+    
+    
+
     useEffect(() => {
         setDropdownItem(dropdownItems[0]);
-    }, [dropdownItems]);
+    }, [dropdownItems]); */
+    
 
     return (
-        <div className="grid">
-            <div className="col-12 md:col-6">
+        <div className="grid ">
+            <div className="col-12 md:col-6 ">
                 <div className="card p-fluid">
-                    <h5>Vertical</h5>
+                    <h5>Profil</h5>
                     <div className="field">
-                        <label htmlFor="name1">Name</label>
-                        <InputText id="name1" type="text" />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="email1">Email</label>
-                        <InputText id="email1" type="text" />
+                        <label htmlFor="name1">Nom</label>
+                        <InputText id="name1" type="text"  value={userObject.firstname } onChange={(e) => handleInputChange('firstname', e.target.value)}  />
                     </div>
                     <div className="field">
-                        <label htmlFor="age1">Age</label>
-                        <InputText id="age1" type="text" />
+                        <label htmlFor="prenom1">Prènom</label>
+                        <InputText id="prenom1" type="text" value={userObject.lastname } onChange={(e) => handleInputChange('lastname', e.target.value)}/>
                     </div>
-                </div>
-
-                <div className="card p-fluid">
-                    <h5>Vertical Grid</h5>
-                    <div className="formgrid grid">
-                        <div className="field col">
-                            <label htmlFor="name2">Name</label>
-                            <InputText id="name2" type="text" />
-                        </div>
-                        <div className="field col">
-                            <label htmlFor="email2">Email</label>
-                            <InputText id="email2" type="text" />
-                        </div>
+                    <div className="field">
+                        <label htmlFor="adresse1">Adresse</label>
+                        <InputText id="adresse1" type="text" value={userObject.adresse } onChange={(e) => handleInputChange('adresse', e.target.value)}/>
                     </div>
-                </div>
-            </div>
-
-            <div className="col-12 md:col-6">
-                <div className="card p-fluid">
-                    <h5>Horizontal</h5>
-                    <div className="field grid">
-                        <label htmlFor="name3" className="col-12 mb-2 md:col-2 md:mb-0">
-                            Name
-                        </label>
-                        <div className="col-12 md:col-10">
-                            <InputText id="name3" type="text" />
-                        </div>
+                    <div className="field">
+                        <label htmlFor="tel1">Téléphone</label>
+                        <InputText id="tel1" type="text" value={userObject.phoneNumber } onChange={(e) => handleInputChange('phoneNumber', e.target.value)}/>
                     </div>
-                    <div className="field grid">
-                        <label htmlFor="email3" className="col-12 mb-2 md:col-2 md:mb-0">
-                            Email
-                        </label>
-                        <div className="col-12 md:col-10">
-                            <InputText id="email3" type="text" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <h5>Inline</h5>
-                    <div className="formgroup-inline">
-                        <div className="field">
-                            <label htmlFor="firstname1" className="p-sr-only">
-                                Firstname
-                            </label>
-                            <InputText id="firstname1" type="text" placeholder="Firstname" />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="lastname1" className="p-sr-only">
-                                Lastname
-                            </label>
-                            <InputText id="lastname1" type="text" placeholder="Lastname" />
-                        </div>
-                        <Button label="Submit"></Button>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <h5>Help Text</h5>
-                    <div className="field p-fluid">
-                        <label htmlFor="username">Username</label>
-                        <InputText id="username" type="text" />
-                        <small>Enter your username to reset your password.</small>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-12">
-                <div className="card">
-                    <h5>Advanced</h5>
-                    <div className="p-fluid formgrid grid">
-                        <div className="field col-12 md:col-6">
-                            <label htmlFor="firstname2">Firstname</label>
-                            <InputText id="firstname2" type="text" />
-                        </div>
-                        <div className="field col-12 md:col-6">
-                            <label htmlFor="lastname2">Lastname</label>
-                            <InputText id="lastname2" type="text" />
-                        </div>
-                        <div className="field col-12">
-                            <label htmlFor="address">Address</label>
-                            <InputTextarea id="address" rows={4} />
-                        </div>
-                        <div className="field col-12 md:col-6">
-                            <label htmlFor="city">City</label>
-                            <InputText id="city" type="text" />
-                        </div>
-                        <div className="field col-12 md:col-3">
-                            <label htmlFor="state">State</label>
-                            <Dropdown id="state" value={dropdownItem} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown>
-                        </div>
-                        <div className="field col-12 md:col-3">
-                            <label htmlFor="zip">Zip</label>
-                            <InputText id="zip" type="text" />
-                        </div>
+                    <div className="field">
+                        <Button label="Sauvegarder" className="p-button-purple" onClick={()=>handleSave(userObject.idUser)}/>
                     </div>
                 </div>
             </div>
